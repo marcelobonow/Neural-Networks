1177846ba6cef3e677916d927236b40934149d55
Marcelo Bonow <marcelobonowott@hotmail.com>
2019-09-05 07:39:22 -0300

    Correção no script baseado no resultado do teste.

--- Redes/Redes.py
+++ Redes/Redes.py
@@ -13,9 +13,9 @@ def activation(value):
         return 1
 def GetClass(value):
     if value == - 1:
-        return "A"
+        return "P1"
     else:
-        return "B"
+        return "P2"
 
 data = pd.read_csv('dados.csv', sep=';')
 maxEpocas = 1000
@@ -23,16 +23,16 @@ maxEpocas = 1000
 values = data[data.columns[0:3]]
 label = data[data.columns[3]]
 
-x_train, x_test, y_train, y_test = train_test_split(values, label, test_size = .3, random_state = 20)
+x_train, x_test, y_train, y_test = train_test_split(values, label, test_size = .3, random_state = 13)
 
 trainQuantity = len(x_train)
 testQuantity = len(x_test)
 
 print("Iniciando treinamento com " + str(trainQuantity) + " amostras \n")
 print("Entradas de treino \n" + x_train.to_string())
-print("Labels de teste \n" + y_train.to_string())
+print("Labels de treino \n" + y_train.to_string())
 print("Entradas de teste \n" + x_test.to_string())
-print("Labels de treino \n" + y_test.to_string())
+print("Labels de teste \n" + y_test.to_string())
 
 bias = 1
 learnRate = 0.01
@@ -47,13 +47,13 @@ for j in range(maxEpocas):
     for i in range(trainQuantity):
         xb = np.hstack((bias, x_train.iloc[i].values))
         
-        v = np.dot(w, xb)
+        v = np.dot(w[0], xb)
         yr = activation(v)
         error = y_train.iloc[i] - yr
         errors[i] = error
         if error != 0:
             finished = False
-        w = w + learnRate * errors[i] * xb
+        w[0] = w[0] + learnRate * error * xb
     if finished:
         print ("Esta sem erros na epoca: " + str(j))
         completedTrain = True
@@ -63,14 +63,13 @@ print("Erros: " + str(errors))
 print("Pesos: " + str(w))
 print ("Iniciando teste\n\n")
 
-print("x0\tx1\tx2\ty\tlabel")
+print("x0\tx1\tx2\ty\tlabel\t")
 for i in range(testQuantity):
-    row = x_test.iloc[i]
     label = y_test.iloc[i]
-    xb = np.hstack((bias, row.values))
-    v = np.dot(w, xb)
+    xb = np.hstack((bias, x_test.iloc[i].values))
+    v = np.dot(w[0], xb)
     y = activation(v)
 
-    print(str(row[0]) + "\t" + str(row[1]) + "\t" + str(row[2]) + "\t" + GetClass(y) + "\t" + GetClass(y))
+    print(str(x_test.iloc[i][0]) + "\t" + str(x_test.iloc[i][1]) + "\t" + str(x_test.iloc[i][2]) + "\t" + GetClass(y) + "\t" + GetClass(label))
 
 f.close()
