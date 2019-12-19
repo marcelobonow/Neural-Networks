90a28981e7701e27c60e49edcc922482d444b595
Marcelo Ott <marcelo.ott@izyplay.com.br>
2019-12-19 20:30:28 -0300

    Base dos treinamentos

--- digitos/digitos/ClassificadorMNIST.py
+++ digitos/digitos/ClassificadorMNIST.py
@@ -3,24 +3,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import KFold
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from numpy import savetxt
 
-mlp = MLPClassifier(hidden_layer_sizes=(1000,), max_iter=10000,solver='sgd', verbose=True, tol=1e-4, learning_rate_init=0.0001)
+mlp = MLPClassifier(hidden_layer_sizes=(1500, 1200), max_iter=10000,solver='sgd', verbose=True, tol=1e-4, learning_rate_init=0.0001)
 
+#lista_dados_train = np.array(pd.read_csv("mnist_10000.csv", sep=','))
+#lista_dados_train = np.array(pd.read_csv("mnist_60000.csv", sep=','))
 lista_dados_train = np.array(pd.read_csv("mnist_10000.csv", sep=','))
-#lista_dados_train=np.array(pd.read_csv("mnist_60000.csv", sep=','))
 y_train = lista_dados_train[:,0]
 X_train = lista_dados_train[:,1:785]
 
-lista_dados_test = np.array(pd.read_csv("mnist_100.csv", sep=','))
-#lista_dados_test = np.array(pd.read_csv("mnist_10000.csv", sep=','))
-y_test = lista_dados_test[:,0]
-X_test = lista_dados_test[:,1:785]
+#lista_dados_test = np.array(pd.read_csv("mnist_100.csv", sep=','))
+lista_dados_test = np.array(pd.read_csv("mnist_10000.csv", sep=','))
+
 
 #mlp.fit(X_train, y_train)
-kf = KFold(n_splits=10)
+kf = KFold(n_splits=3)
 kf.get_n_splits(lista_dados_test)
 for train_index, test_index in kf.split(lista_dados_test):
 	y_train = lista_dados_test[train_index][:,0]
@@ -28,10 +29,11 @@ for train_index, test_index in kf.split(lista_dados_test):
 	y_test = lista_dados_test[test_index][:,0]
 	X_test = lista_dados_test[test_index][:,1:785]
 	mlp.fit(X_train, y_train)
-	print("Current KFolds score: %f" % mlp.score(X_train, y_train))
+	print("Current KFolds score: %f" % mlp.score(X_test, y_test))
 
 
-
+y_test = lista_dados_test[:,0]
+X_test = lista_dados_test[:,1:785]
 print("Training set score: %f" % mlp.score(X_train, y_train))
 
 print("Test set score: %f" % mlp.score(X_test, y_test))
@@ -39,7 +41,7 @@ print("Test set score: %f" % mlp.score(X_test, y_test))
 saida = mlp.predict(X_test)
 pesos = mlp.coefs_
 for i in range(len(pesos)):
-	savetxt('pesos.csv', pesos[i], delimiter=',')
+	savetxt('pesos' + str(i) + '.csv', pesos[i], delimiter=',')
 
 print("Rotulo: ", y_test)
 print(" Predicao: ", saida)
@@ -50,3 +52,4 @@ print("Rotulo: ", y_test[i])
 print("Predicao: ", saida[i])
 
 print(confusion_matrix(y_test, saida))
+plt.show();
--- digitos/digitos/digitos.py
+++ digitos/digitos/digitos.py
@@ -1,1 +1,10 @@
+from sklearn.neural_network import MLPClassifier
 
+
+
+def RecognizeDigit(digit):
+    mlp = MLPClassifier(hidden_layer_sizes=(1500, 1200), max_iter=10000,solver='sgd', verbose=True, tol=1e-4, learning_rate_init=0.0001)
+    peso0 = np.array(pd.read_csv("peso0.csv", sep=','))
+    peso1 = np.array(pd.read_csv("peso1.csv", sep=','))
+    peso2 = np.array(pd.read_csv("peso2.csv", sep=','))
+    return -1
